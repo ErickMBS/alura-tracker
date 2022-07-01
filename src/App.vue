6a189bc7c6b2aa82115e3083c630f3a37f01eb59
erick_braga <@Erickmatheus07>
2022-07-01 12:10:14 -0300

    First commit

---
+++ src/App.vue
@@ -0,0 +1,17 @@
+<template>
+  <main class="columns is-gapless is-multiline">
+    
+  </main>
+</template>
+
+<script lang="ts">
+import { defineComponent } from 'vue';
+
+export default defineComponent({
+  name: 'App',
+});
+</script>
+
+<style>
+
+</style>
