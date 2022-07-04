7932c1e76b1b00f2bca2e94c2f7c8693a0714986
erick_braga <@Erickmatheus07>
2022-07-04 08:21:05 -0300

    Finalizado cronometro e criacao de novos componentes

---
+++ src/components/Formulario.vue
@@ -0,0 +1,45 @@
+<template>
+  <div class="box">
+    <div class="columns">
+      <div
+        class="column is-8"
+        role="form"
+        aria-label="Formulário para criação de uma nova tarefa"
+      >
+        <input
+          type="text"
+          class="input"
+          placeholder="Qual tarefa você deseja iniciar?"
+          v-model="descricao"
+        />
+      </div>
+      <div class="column">
+        <Temporizador @aoTemporizadorFinalizado="finalizarTarefa" />
+      </div>
+    </div>
+  </div>
+</template>
+
+<script lang="ts">
+import { defineComponent } from "vue";
+import Temporizador from "./Temporizador.vue";
+
+export default defineComponent({
+  name: "FormularioTarefa",
+  components: {
+    Temporizador,
+  },
+  data() {
+    return {
+      descricao: "",
+    };
+  },
+  methods: {
+    finalizarTarefa(tempoDecorrido: number): void {
+      console.log(tempoDecorrido);
+      console.log(this.descricao);
+      this.descricao = "";
+    },
+  },
+});
+</script>
