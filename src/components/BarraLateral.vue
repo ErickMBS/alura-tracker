0f049280125e8065464e31c810d5a0cd4a80b8de
erick.braga <erick.braga@laqus.com.br>
2022-07-19 08:32:53 -0300

    Adicionar vue-router e views

--- src/components/BarraLateral.vue
+++ src/components/BarraLateral.vue
@@ -1,11 +1,31 @@
 <template>
   <header>
-      <h1>
-        <img src="../assets/logo.png" alt="">  
-      </h1>
+    <h1>
+      <img src="../assets/logo.png" alt="">
+    </h1>
+    <div class="has-text-centered">
       <button class="button" @click="alterarTema">
         {{ textoBotao }}
       </button>
+    </div>
+    <nav class="panel mt-5">
+      <ul>
+        <li>
+          <router-link to="/" class="link">
+            <i class="fas fa-tasks">
+              tarefas
+            </i>
+          </router-link>
+        </li>
+        <li>
+          <router-link to="/projetos" class="link">
+            <i class="fas fa-project-diagram">
+              projetos
+            </i>
+          </router-link>
+        </li>
+      </ul>
+    </nav>
   </header>
 </template>
 
@@ -14,13 +34,13 @@ import { defineComponent } from 'vue'
 export default defineComponent({
   name: 'BarraLateral',
   emits: ['aoTemaAlterado'],
-  data () {
+  data() {
     return {
       modoEscuroAtivo: false
     }
   },
   computed: {
-    textoBotao () {
+    textoBotao() {
       if (this.modoEscuroAtivo) {
         return 'Desativar modo escuro'
       }
@@ -28,7 +48,7 @@ export default defineComponent({
     }
   },
   methods: {
-    alterarTema () {
+    alterarTema() {
       this.modoEscuroAtivo = !this.modoEscuroAtivo
       this.$emit('aoTemaAlterado', this.modoEscuroAtivo)
     }
@@ -44,10 +64,24 @@ header {
   height: 100vh;
   text-align: center;
 }
+
 @media only screen and (max-width: 768px) {
   header {
     padding: 2.5rem;
     height: auto;
   }
+}
+
+.panel li {
+    margin: 8px 0;
+}
+.link {
+    color: #fff;
+}
+.link:hover {
+    color: #FAF0CA;
+}
+.link.router-link-active {
+    color: #FAF0CA;
 }
 </style>
