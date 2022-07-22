9c157d4c6d80062cce1a1dbb6938ac4a6f5d0e78
erick.braga <erick.braga@laqus.com.br>
2022-07-22 13:16:07 -0300

    Exibir projeto na lista de tarefas

--- src/components/Tarefa.vue
+++ src/components/Tarefa.vue
@@ -1,9 +1,14 @@
 <template>
   <Box>
     <div class="columns">
-      <div class="column is-7">{{ tarefa.descricao || 'Tarefa sem descrição' }}</div>
+      <div class="column is-4">
+        {{ tarefa.descricao || "Tarefa sem descrição" }}
+      </div>
+      <div class="column is-3">
+        {{ tarefa.projeto?.nome || "N/D" }}
+      </div>
       <div class="column">
-        <Cronometro :tempoEmSegundos="tarefa.duracaoEmSegundos"/>
+        <Cronometro :tempoEmSegundos="tarefa.duracaoEmSegundos" />
       </div>
     </div>
   </Box>
@@ -12,19 +17,19 @@
 <script lang="ts">
 import { defineComponent, PropType } from "vue";
 import Cronometro from "./Cronometro.vue";
-import ITarefa from '../interfaces/ITarefa'
-import Box from './Box.vue'
+import ITarefa from "../interfaces/ITarefa";
+import Box from "./Box.vue";
 export default defineComponent({
   name: "TarefaRegistrada",
   components: {
     Cronometro,
-    Box
+    Box,
   },
   props: {
     tarefa: {
       type: Object as PropType<ITarefa>,
-      required: true
-    }
-  }
+      required: true,
+    },
+  },
 });
 </script>
