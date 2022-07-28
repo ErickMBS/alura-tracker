98f4796e107df26d756e2428f1683ee9113d9f9c
erick.braga <erick.braga@laqus.com.br>
2022-07-28 06:15:30 -0300

    Refatorar Formulario projetos para Composition API

--- src/views/Projetos/Formulario.vue
+++ src/views/Projetos/Formulario.vue
@@ -15,7 +15,7 @@
 <script lang="ts">
 import { TipoNotificacao } from "@/interfaces/INotificacao";
 import { useStore } from "@/store";
-import { defineComponent } from "vue";
+import { defineComponent, ref } from "vue";
 import useNotificador from "@/hooks/notificador";
 import { ALTERAR_PROJETO, CADASTRAR_PROJETO } from "@/store/tipo-acoes";
 
@@ -25,17 +25,6 @@ export default defineComponent({
     id: {
       type: String,
     },
-  },
-  mounted() {
-    if (this.id) {
-      const projeto = this.store.state.projeto.projetos.find((proj) => proj.id == this.id);
-      this.nomeDoProjeto = projeto?.nome || "";
-    }
-  },
-  data() {
-    return {
-      nomeDoProjeto: "",
-    };
   },
   methods: {
     salvar() {
@@ -76,12 +65,21 @@ export default defineComponent({
       this.$router.push("/projetos");
     },
   },
-  setup() {
+  setup(props) {
     const store = useStore();
     const { notificar } = useNotificador();
+
+    const nomeDoProjeto = ref("");
+
+    if (props.id) {
+      const projeto = store.state.projeto.projetos.find((proj) => proj.id == props.id);
+      nomeDoProjeto.value = projeto?.nome || "";
+    }
+    
     return {
       store,
       notificar,
+      nomeDoProjeto
     };
   },
 });
